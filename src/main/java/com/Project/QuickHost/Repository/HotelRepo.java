fdd140eac733f426bd32d2b7dabbc0fdbe875a65
shre2 <shreunq123@gmail.com>
2025-07-26 01:39:58 +0530

    RoomService and Hotel service

--- src/main/java/com/Project/QuickHost/Repository/HotelRepo.java
+++ src/main/java/com/Project/QuickHost/Repository/HotelRepo.java
@@ -1,9 +1,15 @@
 package com.Project.QuickHost.Repository;
 
+import com.Project.QuickHost.Dto.HotelDto;
 import com.Project.QuickHost.Entity.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+
 @Repository
 public interface HotelRepo extends JpaRepository<Hotel,Long> {
+//    List<Hotel> getAllHotel();
+
 }
